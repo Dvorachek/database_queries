19066f7c432510df6d7c22041ff7d0b59df34a5e
Dylan <Dylan.Dvorachek@gmail.com>
2018-04-01 23:07:34 -0700

    added error wrapper to create and updated

--- a4/create_courses.py
+++ a4/create_courses.py
@@ -13,6 +13,37 @@ if len(sys.argv) < 2:
 input_filename = sys.argv[1]
 
 # Open your DB connection here
+psql_user = 'dvorache'  # when I started at uvic the maximum character lenght was 8.. I'm glad it's now been upgraded
+psql_db = 'dvorache'
+psql_password = ''
+psql_server = 'studdb2.csc.uvic.ca'
+psql_port = 5432
+
+conn = psycopg2.connect(dbname=psql_db, user=psql_user, password=psql_password, host=psql_server, port=psql_port)
+
+cursor = conn.cursor()
+
+def err(func, conn):
+    def wrapper(*args, **kwargs):
+        try:
+            func(*args, **kwargs)
+        except psycopg2.ProgrammingError as err:
+            #ProgrammingError is thrown when the database error is related to the format of the query (e.g. syntax error)
+            print("Caught a ProgrammingError:",file=sys.stderr)
+            print(err,file=sys.stderr)
+            conn.rollback()
+        except psycopg2.IntegrityError as err: 
+            #IntegrityError occurs when a constraint (primary key, foreign key, check constraint or trigger constraint) is violated.
+            print("Caught an IntegrityError:",file=sys.stderr)
+            print(err,file=sys.stderr)
+            conn.rollback()
+        except psycopg2.InternalError as err:  
+            #InternalError generally represents a legitimate connection error, but may occur in conjunction with user defined functions.
+            #In particular, InternalError occurs if you attempt to continue using a cursor object after the transaction has been aborted.
+            #(To reset the connection, run conn.rollback() and conn.reset(), then make a new cursor)
+            print("Caught an IntegrityError:",file=sys.stderr)
+            print(err,file=sys.stderr)
+            conn.rollback()
 
 with open(input_filename) as f:
     for row in csv.reader(f):
@@ -27,5 +58,11 @@ with open(input_filename) as f:
         
         #Do something with the data here
         #Make sure to catch any exceptions that occur and roll back the transaction if a database error occurs.
+        err(cursor.execute("insert into courses values( %s );", (code)), conn)
+        err(cursor.execute("insert into course_offerings( %s, %s, %s, %s, %s );", (code, name, term, instructor, capacity)), conn)
+        [err(cursor.execure("insert into course_offerings( %s, %s );", (prerequisites[0], pre_req)), conn) for pre_req in prerequisites[1:]]
         
-        
+      
+conn.commit()
+cursor.close()
+conn.close()
