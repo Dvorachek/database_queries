c177b86c3673aaff2ff7a4e1c4a8c8735c1cee25
Dylan <Dylan.Dvorachek@gmail.com>
2018-04-02 23:06:36 -0700

    added if statement to schema and added inserts/drops

--- a4/add_drop.py
+++ a4/add_drop.py
@@ -13,6 +13,37 @@ if len(sys.argv) < 2:
 input_filename = sys.argv[1]
 
 # Open your DB connection here
+psql_user = 'dvorache'  # when I started at uvic the maximum character lenght was 8.. I'm glad it's now been upgraded
+psql_db = 'dvorache'
+psql_password = ''
+psql_server = 'studdb2.csc.uvic.ca'
+psql_port = 5432
+
+conn = psycopg2.connect(dbname=psql_db, user=psql_user, password=psql_password, host=psql_server, port=psql_port)
+
+cursor = conn.cursor()
+
+def err(func, conn):  # created a wrapper for error handling.. because who would really want to write this more than once?
+    def wrapper(*args, **kwargs):
+        try:
+            func(*args, **kwargs)
+        except psycopg2.ProgrammingError as err:
+            #ProgrammingError is thrown when the database error is related to the format of the query (e.g. syntax error)
+            print("Caught a ProgrammingError:",file=sys.stderr)
+            print(err,file=sys.stderr)
+            conn.rollback()
+        except psycopg2.IntegrityError as err: 
+            #IntegrityError occurs when a constraint (primary key, foreign key, check constraint or trigger constraint) is violated.
+            print("Caught an IntegrityError:",file=sys.stderr)
+            print(err,file=sys.stderr)
+            conn.rollback()
+        except psycopg2.InternalError as err:  
+            #InternalError generally represents a legitimate connection error, but may occur in conjunction with user defined functions.
+            #In particular, InternalError occurs if you attempt to continue using a cursor object after the transaction has been aborted.
+            #(To reset the connection, run conn.rollback() and conn.reset(), then make a new cursor)
+            print("Caught an IntegrityError:",file=sys.stderr)
+            print(err,file=sys.stderr)
+            conn.rollback()
 
 with open(input_filename) as f:
     for row in csv.reader(f):
@@ -26,5 +57,12 @@ with open(input_filename) as f:
         
         #Do something with the data here
         #Make sure to catch any exceptions that occur and roll back the transaction if a database error occurs.
-        
-        
+        if add_or_drop == 'ADD':
+            err(cursor.execute("insert into students values( %s, %s );", (student_name, student_id)), conn)
+            err(cursor.execute("insert into enrollment values( %s, %s, %s );", (student_id, course_code, term)), conn)
+        elif add_or_drop == 'DROP':
+            err(cursor.execute("delete from enrollment where student_id = %s and course_code = %s and term_code = %s;", (student_id, course_code, term)), conn)
+
+conn.commit()
+cursor.close()
+conn.close()
