df62687a14e9dc7934bf250f6b0971a3b1ac5340
Dylan <Dylan.Dvorachek@gmail.com>
2018-04-07 17:33:19 -0700

    added reports for testing

--- a4/report_classlist.py
+++ a4/report_classlist.py
@@ -9,6 +9,31 @@
 
 import psycopg2, sys
 
+def e(func, conn):  # created a wrapper for error handling.. because who would really want to write this more than once?
+    def wrapper(*args, **kwargs):
+        try:
+            func(*args, **kwargs)
+        except psycopg2.ProgrammingError as err:
+            #ProgrammingError is thrown when the database error is related to the format of the query (e.g. syntax error)
+            print("Caught a ProgrammingError:",file=sys.stderr)
+            print(err,file=sys.stderr)
+            conn.rollback()
+            sys.exit(1)
+        except psycopg2.IntegrityError as err: 
+            #IntegrityError occurs when a constraint (primary key, foreign key, check constraint or trigger constraint) is violated.
+            print("Caught an IntegrityError:",file=sys.stderr)
+            print(err,file=sys.stderr)
+            conn.rollback()
+            sys.exit(1)
+        except psycopg2.InternalError as err:  
+            #InternalError generally represents a legitimate connection error, but may occur in conjunction with user defined functions.
+            #In particular, InternalError occurs if you attempt to continue using a cursor object after the transaction has been aborted.
+            #(To reset the connection, run conn.rollback() and conn.reset(), then make a new cursor)
+            print("Caught an IntegrityError:",file=sys.stderr)
+            print(err,file=sys.stderr)
+            conn.rollback()
+            sys.exit(1)
+            
 def print_header(course_code, course_name, term, instructor_name):
     print("Class list for %s (%s)"%(str(course_code), str(course_name)) )
     print("  Term %s"%(str(term), ) )
@@ -24,26 +49,65 @@ def print_footer(total_enrolled, max_capacity):
     print("%s/%s students enrolled"%(str(total_enrolled),str(max_capacity)) )
 
 
-''' The lines below would be helpful in your solution
+#''' The lines below would be helpful in your solution
 if len(sys.argv) < 3:
     print('Usage: %s <course code> <term>'%sys.argv[0], file=sys.stderr)
     sys.exit(0)
     
 course_code, term = sys.argv[1:3]
-'''
+#'''
 
+# Open your DB connection here
+psql_user = 'dvorache'  # when I started at uvic the maximum character lenght was 8.. I'm glad it's now been upgraded
+psql_db = 'dvorache'
+psql_password = 'pineapple'
+psql_server = 'studdb2.csc.uvic.ca'
+psql_port = 5432
+
+conn = psycopg2.connect(dbname=psql_db, user=psql_user, password=psql_password, host=psql_server, port=psql_port)
+
+e(cursor.execute("""select *
+    from course_offering
+    natural join
+    enrollment
+    natural join
+    students
+    natural join
+    grade
+    where course_code = %s and term_code = %s;""", (course_code, term)), conn)  # so many joins
+
+row = cursor.fetchone()
+
+student_id, _, _, course_name, instructor_name, capacity, student_name, grade = row
+
+print_header(course_code, course_name, term, instructor_name)
+
+row_count = 0
+while row:
+    if not grade:
+        grade = None
+    print_row(student_id, student_name, grade)
+    row_count += 1
+    row = cursor.fetchone()
+    student_id, _, _, _, _, _, student_name, grade = row
+    
+print_footer(row_count, capacity)
+
+cursor.close()
+conn.close()
 
 # Mockup: Print a class list for CSC 370
-course_code = 'CSC 370'
-course_name = 'Database Systems'
-course_term = 201801
-instructor_name = 'Bill Bird'
-print_header(course_code, course_name, course_term, instructor_name)
+# course_code = 'CSC 370'
+# course_name = 'Database Systems'
+# course_term = 201801
+# instructor_name = 'Bill Bird'
+# print_header(course_code, course_name, course_term, instructor_name)
 
 #Print records for a few students
-print_row('V00123456', 'Rebecca Raspberry', 81)
-print_row('V00123457', 'Alissa Aubergine', 90)
-print_row('V00123458', 'Neal Naranja', 83)
+# print_row('V00123456', 'Rebecca Raspberry', 81)
+# print_row('V00123457', 'Alissa Aubergine', 90)
+# print_row('V00123458', 'Neal Naranja', 83)
 
 #Print the last line (enrollment/max_capacity)
-print_footer(3,150)
+# print_footer(3,150)
+
--- a4/report_transcript.py
+++ a4/report_transcript.py
@@ -9,7 +9,30 @@
 
 import psycopg2, sys
 
-
+def e(func, conn):  # created a wrapper for error handling.. because who would really want to write this more than once?
+    def wrapper(*args, **kwargs):
+        try:
+            func(*args, **kwargs)
+        except psycopg2.ProgrammingError as err:
+            #ProgrammingError is thrown when the database error is related to the format of the query (e.g. syntax error)
+            print("Caught a ProgrammingError:",file=sys.stderr)
+            print(err,file=sys.stderr)
+            conn.rollback()
+            sys.exit(1)
+        except psycopg2.IntegrityError as err: 
+            #IntegrityError occurs when a constraint (primary key, foreign key, check constraint or trigger constraint) is violated.
+            print("Caught an IntegrityError:",file=sys.stderr)
+            print(err,file=sys.stderr)
+            conn.rollback()
+            sys.exit(1)
+        except psycopg2.InternalError as err:  
+            #InternalError generally represents a legitimate connection error, but may occur in conjunction with user defined functions.
+            #In particular, InternalError occurs if you attempt to continue using a cursor object after the transaction has been aborted.
+            #(To reset the connection, run conn.rollback() and conn.reset(), then make a new cursor)
+            print("Caught an IntegrityError:",file=sys.stderr)
+            print(err,file=sys.stderr)
+            conn.rollback()
+            sys.exit(1)
 
 def print_header(student_id, student_name):
     print("Transcript for %s (%s)"%(str(student_id), str(student_name)) )
@@ -20,21 +43,46 @@ def print_row(course_term, course_code, course_name, grade):
     else:
         print("%6s %10s %-35s   (NO GRADE ASSIGNED)"%(str(course_term), str(course_code), str(course_name)) )
 
-''' The lines below would be helpful in your solution
+#''' The lines below would be helpful in your solution
 if len(sys.argv) < 2:
     print('Usage: %s <student id>'%sys.argv[0], file=sys.stderr)
     sys.exit(0)
     
 student_id = sys.argv[1]
-'''
+# '''
 
+# Open your DB connection here
+psql_user = 'dvorache'  # when I started at uvic the maximum character lenght was 8.. I'm glad it's now been upgraded
+psql_db = 'dvorache'
+psql_password = 'pineapple'
+psql_server = 'studdb2.csc.uvic.ca'
+psql_port = 5432
 
-# Mockup: Print a transcript for V00123456 (Rebecca Raspberry)
-student_id = 'V00123456'
-student_name = 'Rebecca Raspberry'
+conn = psycopg2.connect(dbname=psql_db, user=psql_user, password=psql_password, host=psql_server, port=psql_port)
+
+e(cursor.execute("""select *
+    from students
+    natural join
+    grades
+    where student_id = %s;""", (student_id,)), conn)
+
+row = cursor.fetchone()
+_,student_name,course_code,term,grade = row  # check grade value
+
 print_header(student_id, student_name)
+while row:
+    print_row(term, course_code, course_name, grade)
+    row = cursor.fetchone()
+    _,_,course_code,term,grade = row
+
+cursor.close()
+conn.close()
+# Mockup: Print a transcript for V00123456 (Rebecca Raspberry)
+# student_id = 'V00123456'
+# student_name = 'Rebecca Raspberry'
+# print_header(student_id, student_name)
 
 
-print_row(201709,'CSC 110','Fundamentals of Programming: I', 90)
-print_row(201709,'CSC 187','Recursive Algorithm Design', None) #The special value None is used to indicate that no grade is assigned.
-print_row(201801,'CSC 115','Fundamentals of Programming: II', 75)
+# print_row(201709,'CSC 110','Fundamentals of Programming: I', 90)
+# print_row(201709,'CSC 187','Recursive Algorithm Design', None) #The special value None is used to indicate that no grade is assigned.
+# print_row(201801,'CSC 115','Fundamentals of Programming: II', 75)
