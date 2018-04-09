e4278374401c531ec8f52c6b96b2d347fbb821d0
Dylan John Dvorachek <Dylan.Dvorachek@gmail.com>
2018-04-08 22:07:15 -0700

    Update report_enrollment.py

--- a4/report_enrollment.py
+++ a4/report_enrollment.py
@@ -12,17 +12,36 @@
 
 import psycopg2, sys
 
+def e(func, conn):  # wrapper for error handling
+    def wrapper(*args, **kwargs):
+        try:
+            func(*args, **kwargs)
+        except psycopg2.ProgrammingError as err:
+            #ProgrammingError is thrown when the database error is related to the format of the query (e.g. syntax error)
+            print("Caught a ProgrammingError:",file=sys.stderr)
+            print(err,file=sys.stderr)
+            conn.rollback()
+            sys.exit(0)
+        except psycopg2.IntegrityError as err: 
+            #IntegrityError occurs when a constraint (primary key, foreign key, check constraint or trigger constraint) is violated.
+            print("Caught an IntegrityError:",file=sys.stderr)
+            print(err,file=sys.stderr)
+            conn.rollback()
+            sys.exit(0)
+        except psycopg2.InternalError as err:  
+            #InternalError generally represents a legitimate connection error, but may occur in conjunction with user defined functions.
+            #In particular, InternalError occurs if you attempt to continue using a cursor object after the transaction has been aborted.
+            #(To reset the connection, run conn.rollback() and conn.reset(), then make a new cursor)
+            print("Caught an IntegrityError:",file=sys.stderr)
+            print(err,file=sys.stderr)
+            conn.rollback()
+            sys.exit(0)
+
 def print_row(term, course_code, course_name, instructor_name, total_enrollment, maximum_capacity):
     print("%6s %10s %-35s %-25s %s/%s"%(str(term), str(course_code), str(course_name), str(instructor_name), str(total_enrollment), str(maximum_capacity)) )
 
-# Mockup: Print some data for a few made up classes
-
-# print_row(201709, 'CSC 106', 'The Practice of Computer Science', 'Bill Bird', 203, 215)
-# print_row(201709, 'CSC 110', 'Fundamentals of Programming: I', 'Jens Weber', 166, 200)
-# print_row(201801, 'CSC 370', 'Database Systems', 'Bill Bird', 146, 150)
-
 # Open your DB connection here
-psql_user = 'dvorache'  # when I started at uvic the maximum character lenght was 8.. I'm glad it's now been upgraded
+psql_user = 'dvorache'
 psql_db = 'dvorache'
 psql_password = 'pineapple'
 psql_server = 'studdb2.csc.uvic.ca'
@@ -32,13 +51,13 @@ conn = psycopg2.connect(dbname=psql_db, user=psql_user, password=psql_password, 
 
 cursor = conn.cursor()
 
-cursor.execute("""select P1.course_code, P1.term_code, P1.course_name, P1.instructor_name, count(P2.student_id), P1.capacity
+e(cursor.execute("""select P1.course_code, P1.term_code, P1.course_name, P1.instructor_name, count(P2.student_id), P1.capacity
     from course_offering as P1 left join enrollment as P2
     on P1.course_code = P2.course_code
     and P1.term_code = P2.term_code
     group by P1.course_code, P1.term_code, P1.course_name, P1.instructor_name, P1.capacity
     order by P1.term_code, P1.course_code;
-    """)
+    """), conn)
 
 rows_found = 0
 row = cursor.fetchone()
